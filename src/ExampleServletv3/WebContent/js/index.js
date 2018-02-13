8ff4a77cfe2e9e5129367279b661f5253085d15c
Kamal Nasser <hello@kamal.io>
2018-02-13 13:41:59 +0200

    latest changes

--- src/ExampleServletv3/WebContent/js/index.js
+++ src/ExampleServletv3/WebContent/js/index.js
@@ -29,10 +29,23 @@ app.controller('StateManager', [ '$scope', 'State', function($scope, State) {
 
 } ]);
 
-app.controller('HomeController', [ '$scope', 'State', 'Redirect',
+app.controller('MainController', [ '$scope', 'State', 'Redirect',
 	function($scope, State, Redirect) {
 
 	    $scope.currentPage = 'home';
 	    $scope.redirect = Redirect.bind(this, $scope);
+	    $scope.state = State;
+	    
+	    
 
 	} ]);
+
+app.controller('NavController', [ '$scope', function($scope) {
+
+} ]);
+
+app.controller('HomeController', [ '$scope', function($scope) {
+
+    $scope.name = 'Steve';
+
+} ]);
