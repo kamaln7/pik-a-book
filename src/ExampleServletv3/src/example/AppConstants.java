74249a7b6f230e3153ea3d341aae280d6bb820c9
rawanManasra <rwan.mnasra@gmail.com>
2018-02-13 15:09:07 +0200

    finish tables

--- src/ExampleServletv3/src/example/AppConstants.java
+++ src/ExampleServletv3/src/example/AppConstants.java
@@ -44,6 +44,41 @@ public interface AppConstants {
 			+ "CONSTRAINT primary_key PRIMARY KEY (id)"
 			+ ")";
 	
+	public final String DB_CREATE_TABLE_EBOOKS ="CREATE TABLE ebooks("
+			+"id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),"
+			+"name VARCHAR(500) NUT NULL"
+			+"cover VARCHAR(500) NOT NULL"
+			+"price INTEGER NOT NULL"
+			+"content VARCHAR(20000) NUT NULL"
+			+"discreption VARCHAR(50) NUT NULL"
+			+"timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+			+ "CONSTRAINT primary_key PRIMARY KEY (id)"
+			+ ")";
+	
+	
+	public final String DB_CREATE_TABLE_LIKES ="CREATE TABLE likes ("
+			+"user_id  INTEGER NOT NULL,"
+			+"ebook_id INTEGER NOT NULL,"
+			+"CONSTRAINT primary_key PRIMARY KEY (user_id,ebook_id)"
+			+ ")";
+	
+	public final String DB_CREATE_TABLE_REVIEWS ="CREATE TABLE reviews ("
+			+"id INTEGER NOT NULL,"
+			+"user_id  INTEGER NOT NULL,"
+			+"ebook_id INTEGER NOT NULL,"
+			+"content VARCHAR(100) NOT NULL,"
+			+"is_published INTEGER DEFAULT 0 NOT NULL,"
+			+"timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+			+ "CONSTRAINT primary_key PRIMARY KEY (id)"
+			+ ")" ;
+	public final String DB_CREATE_TABLE_PURCHASES ="CREATE TABLE purchases ("
+			+"id INTEGER NOT NULL,"
+			+"user_id  INTEGER NOT NULL,"
+			+"ebook_id INTEGER NOT NULL,"
+			+"timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+			+ "CONSTRAINT primary_key PRIMARY KEY (id)"
+			+ ")" ;
+	
 	public final String INSERT_CUSTOMER_STMT = "INSERT INTO CUSTOMER VALUES(?,?,?)";
 	public final String SELECT_ALL_CUSTOMERS_STMT = "SELECT * FROM CUSTOMER";
 	public final String SELECT_CUSTOMER_BY_NAME_STMT = "SELECT * FROM CUSTOMER "
