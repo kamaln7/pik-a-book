e567bd7e24546ee1d4428dfb51fc4a1bd9e09d02
Kamal Nasser <hello@kamal.io>
2018-02-15 22:14:53 +0200

    default phone pre

--- src/ExampleServletv3/WebContent/js/controllers/register.js
+++ src/ExampleServletv3/WebContent/js/controllers/register.js
@@ -2,6 +2,7 @@ app.controller('RegisterController', [ '$scope', '$http',
 	function($scope, $http) {
 	    $scope.submitted = false;
 	    $scope.error = "";
+	    $scope.telephonepre = "050";
 
 	    $scope.submit = function() {
 		var telephone = $scope.telephonepre + $scope.phone;
