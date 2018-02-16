716f467feeb8e0dba1049195da6cd4054fa1622f
rawanManasra <rwan.mnasra@gmail.com>
2018-02-16 20:18:46 +0200

    register save data into derby(i wish !)

--- src/ExampleServletv3/src/example/servlets/RegistrationServlet.java
+++ src/ExampleServletv3/src/example/servlets/RegistrationServlet.java
@@ -15,8 +15,11 @@
 package example.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -25,6 +28,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import example.Helpers;
+import example.model.User;
 
 @WebServlet(urlPatterns = { "/auth/registration" })
 public class RegistrationServlet extends HttpServlet {
@@ -39,46 +46,88 @@ public class RegistrationServlet extends HttpServlet {
 
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		final String username = request.getParameter("username");
-		final String password = request.getParameter("password");
-		final String email = request.getParameter("email");
-		final String city = request.getParameter("city");
-		final String street = request.getParameter("street");
-		final String nickname = request.getParameter("nickname");
-		final String bio = request.getParameter("bio");
-		final String photoURL = request.getParameter("photo");
-		final Integer zip = Integer.parseInt(request.getParameter("zip"));
-		final Integer telephone = Integer.parseInt(request.getParameter("phone"));
-		final Integer streetNumber = Integer.parseInt(request.getParameter("sNumber"));
+		String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+		JsonObject input = new JsonParser().parse(body).getAsJsonObject();
+		String username = input.get("username").getAsString(), password = input.get("password").getAsString();
+		String email = input.get("email").getAsString(), fullname = input.get("fullname").getAsString();
+		String telephone = input.get("telephone").getAsString(), city = input.get("city").getAsString();
+		String zip = input.get("zip").getAsString(), street = input.get("street").getAsString();
+		Integer street_number = input.get("street_number").getAsInt();
+		String photo = "";
+		String nickname = "";
+		String bio = "";
+		if (input.get("bio") != null) {
+			bio = input.get("bio").getAsString();
+		}
+		if (input.get("nickname") != null) {
+			nickname = input.get("nickname").getAsString();
+		}
+		if (input.get("photo") != null) {
+			photo = input.get("photo").getAsString();
+		}
+		try {
+			Connection conn = Helpers.getConnection(request.getServletContext());
 
-		if (!email.isEmpty() && !username.isEmpty() && !password.isEmpty() && !city.isEmpty() && !street.isEmpty()) {
-			response.setStatus(HttpServletResponse.SC_OK);
-		} else {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			User user = new User(username, email, password, fullname, street, city, zip, telephone, nickname, bio,
+					photo, false, street_number);
+			Integer userId = user.insert(conn);
+			JsonObject o = new JsonObject();
+			o.addProperty("id", userId);
+			o.addProperty("username", user.username);
+			o.addProperty("email", user.email);
+			o.addProperty("password", user.password);
+			o.addProperty("fullname", user.fullname);
+			o.addProperty("street", user.street);
+			o.addProperty("steet_number", user.street_number);
+			o.addProperty("city", user.city);
+			o.addProperty("zip", user.zip);
+			o.addProperty("telephone", user.telephone);
+			o.addProperty("nickname", user.nickname);
+			o.addProperty("bio", user.bio);
+			o.addProperty("photo", user.photo);
+			o.addProperty("is_admin", user.is_admin);
+			response.getWriter().write(o.toString());
+
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+			response.setStatus(500);
+			Helpers.JSONError("A server error occured", response);
 		}
 
-		// just to illustrate the use of Json in Servlets, we return the input to the
-		// client
-		final JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("username", username);
-		jsonObject.addProperty("email", email);
-		jsonObject.addProperty("password", password);
-		jsonObject.addProperty("city", city);
-		jsonObject.addProperty("street", street);
-		jsonObject.addProperty("streetNumber", streetNumber);
-		jsonObject.addProperty("zip", zip);
-		jsonObject.addProperty("telephone", telephone);
-		jsonObject.addProperty("nickname", nickname);
-		jsonObject.addProperty("bio", bio);
-		jsonObject.addProperty("photo", photoURL);
-
-		response.setContentType("application/json");
-		System.out.println(username);
-		// Get the printwriter object from response to write the required json object to
-		// the output stream
-		final PrintWriter out = response.getWriter();
-		// Assuming your json object is **jsonObject**, perform the following, it will
-		// return your json object
-		out.print(jsonObject.toString());
+		/*
+		 * final String username = request.getParameter("username"); final String
+		 * password = request.getParameter("password"); final String email =
+		 * request.getParameter("email"); final String city =
+		 * request.getParameter("city"); final String street =
+		 * request.getParameter("street"); final String nickname =
+		 * request.getParameter("nickname"); final String bio =
+		 * request.getParameter("bio"); final String photoURL =
+		 * request.getParameter("photo"); final Integer zip =
+		 * Integer.parseInt(request.getParameter("zip")); final Integer telephone =
+		 * Integer.parseInt(request.getParameter("phone")); final Integer streetNumber =
+		 * Integer.parseInt(request.getParameter("sNumber"));
+		 * 
+		 * if (!email.isEmpty() && !username.isEmpty() && !password.isEmpty() &&
+		 * !city.isEmpty() && !street.isEmpty()) {
+		 * response.setStatus(HttpServletResponse.SC_OK); } else {
+		 * response.setStatus(HttpServletResponse.SC_FORBIDDEN); }
+		 * 
+		 * // just to illustrate the use of Json in Servlets, we return the input to the
+		 * // client final JsonObject jsonObject = new JsonObject();
+		 * jsonObject.addProperty("username", username); jsonObject.addProperty("email",
+		 * email); jsonObject.addProperty("password", password);
+		 * jsonObject.addProperty("city", city); jsonObject.addProperty("street",
+		 * street); jsonObject.addProperty("streetNumber", streetNumber);
+		 * jsonObject.addProperty("zip", zip); jsonObject.addProperty("telephone",
+		 * telephone); jsonObject.addProperty("nickname", nickname);
+		 * jsonObject.addProperty("bio", bio); jsonObject.addProperty("photo",
+		 * photoURL);
+		 * 
+		 * response.setContentType("application/json"); // Get the printwriter object
+		 * from response to write the required json object to // the output stream final
+		 * PrintWriter out = response.getWriter(); // Assuming your json object is
+		 * **jsonObject**, perform the following, it will // return your json object
+		 * out.print(jsonObject.toString());
+		 */
 	}
 }
