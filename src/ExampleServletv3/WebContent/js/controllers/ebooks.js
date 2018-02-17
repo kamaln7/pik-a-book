7c139383b8ceee26852a67abd55d816001c2f98a
Kamal Nasser <hello@kamal.io>
2018-02-17 15:25:01 +0200

    seperate ebooks page

---
+++ src/ExampleServletv3/WebContent/js/controllers/ebooks.js
@@ -0,0 +1,13 @@
+app.controller('EbooksController', [
+	'$scope',
+	'$http',
+	function($scope, $http) {
+	    $http.get(apiUrl + "/ebooks").then(
+		    function(res) {
+			$scope.ebooks = res.data;
+		    },
+		    function(res) {
+			$scope.error = res.data ? res.data.message
+				: 'A server error occurred';
+		    });
+	} ]);
--- src/ExampleServletv3/WebContent/js/controllers/home.js
+++ src/ExampleServletv3/WebContent/js/controllers/home.js
@@ -1,7 +1,3 @@
-app.controller('HomeController', [ '$scope', '$http', function($scope, $http) {
-    $http.get(apiUrl + "/ebooks").then(function(res) {
-	$scope.ebooks = res.data;
-    }, function(res) {
-	$scope.error = res.data ? res.data.message : 'A server error occurred';
-    });
+app.controller('HomeController', [ '$scope', function($scope) {
+
 } ]);
