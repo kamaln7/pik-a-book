33062644528f4a2d976474038eb67d07caf2c147
rawanManasra <rwan.mnasra@gmail.com>
2018-02-18 17:56:54 +0200

    scrolling - stam bs ht7la

--- src/ExampleServletv3/WebContent/js/controllers/ebooks.js
+++ src/ExampleServletv3/WebContent/js/controllers/ebooks.js
@@ -1,7 +1,9 @@
 app.controller('EbooksController', [
 	'$scope',
 	'$http',
-	function($scope, $http) {
+	'$location',
+	'$anchorScroll',
+	function($scope, $http, $location, $anchorScroll) {
 	    $http.get(apiUrl + "/ebooks").then(
 		    function(res) {
 			$scope.ebooks = res.data;
@@ -9,5 +11,11 @@ app.controller('EbooksController', [
 		    function(res) {
 			$scope.error = res.data ? res.data.message
 				: 'A server error occurred';
-		    });
+		    }, $scope.scrollTo = function(scrollLocation) {
+			$location.hash(scrollLocation);
+			$anchorScroll();
+
+		    }
+
+	    );
 	} ]);
