ee5a30eec240321dd32be5244144247f657fef64
rawanManasra <rwan.mnasra@gmail.com>
2018-02-19 22:32:30 +0200

    kk

--- src/ExampleServletv3/WebContent/js/controllers/ebooks.js
+++ src/ExampleServletv3/WebContent/js/controllers/ebooks.js
@@ -3,6 +3,7 @@ app.controller('EbooksController', [
 	'$http',
 	'$location',
 	'$anchorScroll',
+	'$locate',
 	function($scope, $http, $location, $anchorScroll) {
 	    $http.get(apiUrl + '/ebooks').then(
 		    function(res) {
@@ -18,4 +19,20 @@ app.controller('EbooksController', [
 		$anchorScroll();
 		console.log(scrollLocation);
 	    }
-	} ]);
+
+	}, function($scope, $locale) {
+	    $scope.currentYear = new Date().getFullYear();
+	    $scope.currentMonth = new Date().getMonth() + 1;
+	    $scope.months = $locale.DATETIME_FORMATS.MONTH;
+	    $scope.paymentInfo = {
+		type : undefined
+	    }
+	    $scope.save = function(data) {
+		if ($scope.paymentForm.$valid) {
+		    console.log(data) // valid data saving stuff here
+		}
+	    }
+
+	}
+
+]);
