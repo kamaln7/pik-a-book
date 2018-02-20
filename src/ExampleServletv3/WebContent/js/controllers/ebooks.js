8f8d25aeadcf8bb9dc69588c73b5454134436453
Kamal Nasser <hello@kamal.io>
2018-02-20 22:52:04 +0200

    remove console log

--- src/ExampleServletv3/WebContent/js/controllers/ebooks.js
+++ src/ExampleServletv3/WebContent/js/controllers/ebooks.js
@@ -16,7 +16,6 @@ app.controller('EbooksController', [
 	    $scope.scrollTo = function(scrollLocation) {
 		$location.hash(scrollLocation);
 		$anchorScroll();
-		console.log(scrollLocation);
 	    }
 
 	    $scope.redirectToEbook = function(id) {
