8b82d800ca9fdde8f8f084788898d38d83bf313a
Kamal Nasser <hello@kamal.io>
2018-02-22 14:10:08 +0200

    bind book as actual html - doesnt work

--- src/ExampleServletv3/WebContent/js/controllers/ebookread.js
+++ src/ExampleServletv3/WebContent/js/controllers/ebookread.js
@@ -1,31 +1,52 @@
-app.controller('EbookReadController', [
-	'$scope',
-	'$http',
-	'$sce',
-	function($scope, $http, $sce) {
-	    data = $scope.getRedirectData();
-	    $http.get(apiUrl + '/ebooks/' + data.id)
-		    .then(
-			    function(res) {
-				$scope.book = res.data;
-				if ($scope.book.has_purchased) {
-				    $scope.book.iframe_url = $sce
-					    .trustAsResourceUrl('./books/'
-						    + $scope.book.path
-						    + '/ebook.html');
-				}
-			    },
-			    function(res) {
-				$scope.error = res.data ? res.data.message
-					: 'A server error occurred';
-			    });
+app
+	.controller(
+		'EbookReadController',
+		[
+			'$scope',
+			'$http',
+			'$sce',
+			function($scope, $http, $sce) {
+			    data = $scope.getRedirectData();
+			    $http
+				    .get(apiUrl + '/ebooks/' + data.id)
+				    .then(
+					    function(res) {
+						$scope.book = res.data;
+						if ($scope.book.has_purchased) {
+						    $scope.book.iframe_url = $sce
+							    .trustAsResourceUrl('./books/'
+								    + $scope.book.path
+								    + '/ebook.html');
+						    $http
+							    .get(
+								    './books/'
+									    + $scope.book.path
+									    + '/ebook.html')
+							    .then(
+								    function(
+									    res) {
+									$scope.bookHTML = $sce
+										.trustAsHtml(res.data);
+								    },
+								    function(
+									    res) {
+									$scope.error = res.data ? res.data.message
+										: 'A server error occurred';
+								    });
+						}
 
-	    $scope.redirectBack = function() {
-		$scope.setRedirectData({
-		    id : data.id,
-		});
+					    },
+					    function(res) {
+						$scope.error = res.data ? res.data.message
+							: 'A server error occurred';
+					    });
 
-		$scope.redirect('ebooks.ebook');
-	    }
+			    $scope.redirectBack = function() {
+				$scope.setRedirectData({
+				    id : data.id,
+				});
 
-	} ]);
+				$scope.redirect('ebooks.ebook');
+			    }
+
+			} ]);
