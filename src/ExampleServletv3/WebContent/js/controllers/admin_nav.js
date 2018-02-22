6e6205dbb37399128e088c51a1bc6e0f1d7c0b2a
Kamal Nasser <hello@kamal.io>
2018-02-22 14:21:06 +0200

    fix file names

--- src/ExampleServletv3/WebContent/js/controllers/admin_nav.js
+++ src/ExampleServletv3/WebContent/js/controllers/admin_nav.js
@@ -1,10 +1,2 @@
-app.controller('NavController', [ '$scope', '$http', function($scope, $http) {
-    $scope.logout = function() {
-	$http.get(apiUrl + '/auth/logout').then(function() {
-	    $scope.reloadAuthState();
-	    $scope.redirect('home');
-	}, function() {
-	    window.location.reload();
-	});
-    }
+app.controller('AdminNavController', [ '$scope', function($scope) {
 } ]);
--- src/ExampleServletv3/WebContent/js/controllers/nav.js
+++ src/ExampleServletv3/WebContent/js/controllers/nav.js
@@ -1,2 +1,10 @@
-app.controller('AdminNavController', [ '$scope', function($scope) {
+app.controller('NavController', [ '$scope', '$http', function($scope, $http) {
+    $scope.logout = function() {
+	$http.get(apiUrl + '/auth/logout').then(function() {
+	    $scope.reloadAuthState();
+	    $scope.redirect('home');
+	}, function() {
+	    window.location.reload();
+	});
+    }
 } ]);
