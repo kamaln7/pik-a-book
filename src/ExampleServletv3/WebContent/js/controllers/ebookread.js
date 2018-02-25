d8043da25731d49236b7db9a249aa5b9d9f6cda9
Kamal Nasser <hello@kamal.io>
2018-02-25 16:25:02 +0200

    make remember-scrolling work on chrome

--- src/ExampleServletv3/WebContent/js/controllers/ebookread.js
+++ src/ExampleServletv3/WebContent/js/controllers/ebookread.js
@@ -1,86 +1,99 @@
 var readEbookScrollStuff = {
-    handler : function() {
+    handler: function() {},
+    forward: function() {
+        this.handler.apply(this, arguments);
     },
-    forward : function() {
-	this.handler.apply(this, arguments);
-    },
-    active : true,
-    timeOut : function() {
-	setTimeout(function() {
-	    this.active = true;
-	}.bind(this), 600);
+    active: true,
+    timeOut: function() {
+        setTimeout(function() {
+            this.active = true;
+        }.bind(this), 600);
     },
 };
 
 window.document.addEventListener('scroll', readEbookScrollStuff.forward
-	.bind(readEbookScrollStuff), true);
+    .bind(readEbookScrollStuff), true);
 
 app.controller('EbookReadController', [
-	'$scope',
-	'$http',
-	'$sce',
-	'$document',
-	function($scope, $http, $sce, $document) {
-	    data = $scope.getRedirectData();
-	    $scope.pos = 0;
-	    var ebookId = data.id;
+    '$scope',
+    '$http',
+    '$sce',
+    function($scope, $http, $sce) {
+        data = $scope.getRedirectData();
+        $scope.pos = 0;
+        var ebookId = data.id;
 
-	    readEbookScrollStuff.handler = function(scope, e) {
-		if (!this.active || scope.currentPage != 'ebooks.read'
-			|| e.pageY == 0)
-		    return;
+        readEbookScrollStuff.handler = function(scope, e) {
+            var pos = e.pageY || (e.srcElement && e.srcElement.scrollingElement && e.srcElement.scrollingElement.scrollTop) || 0;
+            if (!this.active || scope.currentPage != 'ebooks.read' ||
+                pos == 0)
+                return;
 
-		scope.$apply(function() {
-		    scope.pos = e.pageY;
-		});
-		this.active = false;
-	    }.bind(readEbookScrollStuff, $scope);
+            scope.$apply(function() {
+                scope.pos = pos;
+            });
+            this.active = false;
+        }.bind(readEbookScrollStuff, $scope);
 
-	    $scope.$watch('pos', function(val) {
-		if (val == 0)
-		    return;
+        $scope.$watch('pos', function(val) {
+            if (val == 0)
+                return;
 
-		$http.post(apiUrl + '/ebooks/scrolling-position/' + ebookId, {
-		    position : val,
-		}).then(function(res) {
-		    readEbookScrollStuff.timeOut.bind(readEbookScrollStuff)();
-		}, function(res) {
-		    readEbookScrollStuff.timeOut.bind(readEbookScrollStuff)();
-		});
-	    });
+            $http.post(apiUrl + '/ebooks/scrolling-position/' + ebookId, {
+                position: val,
+            }).finally(function() {
+                readEbookScrollStuff.timeOut.bind(readEbookScrollStuff)();
+            });
+        });
 
-	    var getReadingPosition = function() {
-		$http.get(apiUrl + '/ebooks/scrolling-position/' + ebookId)
-			.then(function(res) {
-			    window.scroll(0, res.data.position);
-			}, function(res) {
+        var getReadingPosition = function() {
+            $http.get(apiUrl + '/ebooks/scrolling-position/' + ebookId)
+                .then(function(res) {
+                    window.scroll(0, res.data.position);
+                }, function(res) {
 
-			});
-	    };
+                });
+        };
 
-	    $http.get(apiUrl + '/ebooks/' + ebookId)
-		    .then(
-			    function(res) {
-				$scope.book = res.data;
-				if ($scope.book.has_purchased) {
-				    $scope.book.iframe_url = $sce
-					    .trustAsResourceUrl('./books/'
-						    + $scope.book.path
-						    + '/ebook.html');
-				    setTimeout(getReadingPosition, 250);
-				}
-			    },
-			    function(res) {
-				$scope.gshowError(res.data ? res.data.message
-					: 'A server error occurred', '', true);
-			    });
+        var getSH = function() {
+            return $('body')[0].scrollHeight;
+        };
+        var initialScrollHeight = getSH(),
+            checkSHandScroll = function() {
+                console.log(getSH(), (initialScrollHeight + 1000));
+                if (getSH() > (initialScrollHeight + 1000)) {
+                    setTimeout(getReadingPosition, 50);
+                } else {
+                    setTimeout(checkSHandScroll, 100)
+                }
+            };
 
-	    $scope.redirectBack = function() {
-		$scope.setRedirectData({
-		    id : ebookId,
-		});
+        $http.get(apiUrl + '/ebooks/' + ebookId)
+            .then(
+                function(res) {
+                    $scope.book = res.data;
+                    if ($scope.book.has_purchased) {
+                        $scope.book.iframe_url = $sce
+                            .trustAsResourceUrl('./books/' +
+                                $scope.book.path +
+                                '/ebook.html');
 
-		$scope.redirect('ebooks.ebook');
-	    }
+                        // setTimeout(getReadingPosition, 600);
+                        setTimeout(checkSHandScroll, 100)
+                    }
+                },
+                function(res) {
+                    $scope.gshowError(res.data ? res.data.message :
+                        'A server error occurred', '', true);
+                });
 
-	} ]);
+        $scope.redirectBack = function() {
+            $scope.setRedirectData({
+                id: ebookId,
+            });
+
+            $scope.redirect('ebooks.ebook');
+        }
+
+    }
+]);
